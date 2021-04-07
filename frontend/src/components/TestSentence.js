c52eb5cf892b670c7a02a8af196a1ebbb87fdd93
Lukasz Kowalik <lukaszkowalik@Lukaszs-MacBook-Pro.local>
2021-04-07 19:52:30 +0100

    capturing user input

--- frontend/src/components/TestSentence.js
+++ frontend/src/components/TestSentence.js
@@ -1,5 +1,6 @@
 import {useParams} from "react-router-dom";
 import {useSelector} from "react-redux";
+import { useEffect, useState } from "react";
 
 const TestSentence  = () => {
     const {id}=useParams()
@@ -7,11 +8,28 @@ const TestSentence  = () => {
     const currentSentence=sentences.find(sentence=>sentence.id==id);
     const text=currentSentence.text;
     
-    const parts=text.split("");
+    const handleKeyPress=(e)=>{
+        if ((e.key.length==1) && (e.key!=" ")){
+            const newLetter=e.key.toLowerCase();
+            setParts(parts=>parts.map((part)=>{
+                if (part.letter.toLowerCase()==newLetter){
+                    return {letter:part.letter,visible:true}
+                }
+                return part;
+            }))    
+        }
+    }
+    useEffect(()=>{
+        window.addEventListener("keyup",handleKeyPress)
+        return ()=>{window.removeEventListener("keyup",handleKeyPress)}
+    },[])
+    const [parts,setParts]=useState(text.split("").map(letter=>{
+        return {letter:letter,visible:false}
+    }))
     return (
        <div className="test">
          {parts.map((part,index)=>{
-             return <div key={index} className="letter">{part}</div>
+             return <div key={index} className="letter">{part.visible ? part.letter : ""}</div>
          })}
        </div>
        )
