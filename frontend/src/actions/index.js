2119f055d385ac8ea3491e42613689c2f6718273
Lukasz Kowalik <lukaszkowalik@Lukaszs-MacBook-Pro.local>
2021-04-07 18:44:20 +0100

    implementing deleting sentences logic

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -19,3 +19,15 @@ export const create=(text)=>{
             return dispatch({type:"CREATE",payload:data})
         }
 };
+
+export const remove=(id)=>{
+
+    return async (dispatch, getState)=>{
+        const response=await fetch("http://localhost:3001/sentences/"+id,{
+            method: "DELETE",
+            headers: {"Content-Type":"application/json"}
+        });
+
+        return dispatch({type:"DELETE",payload:id})
+    }
+};
--- frontend/src/components/Sentence.js
+++ frontend/src/components/Sentence.js
@@ -1,11 +1,19 @@
+import { useDispatch } from 'react-redux';
 import {Link} from 'react-router-dom';
+import {remove} from '../actions/index.js';
 
 const Sentence=(sentence)=>{
+    const dispatch=useDispatch();
+    const handleDelete=(id)=>{
+        dispatch(remove(id));
+    }
     return (
     <div className="sentence">
         <h3>{sentence.text}</h3>
         <Link className="link" to={'/edit/'+sentence.id}>Edit</Link>
         <Link className="link" to={'/test/'+sentence.id}>Test</Link>
+        <button className="link" onClick={()=>handleDelete(sentence.id)}>Delete</button>
+
     </div>
     )
 };
